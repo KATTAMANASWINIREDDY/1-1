08cddb346d8fe7877888751c8fef55d800a5747d
KATTAMANASWINIREDDY <120378098+KATTAMANASWINIREDDY@users.noreply.github.com>
2023-01-29 10:14:46 +0530

    Add files via upload

---
+++ Typecasting/Typecasting.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+float a,b;
+printf("enter a value:");
+scanf("%f",&a);
+printf("enter b value:");
+scanf("%f",&b);
+int i=(int)a/(int)b;
+printf("The result is %d",i);
+return 0;
+}
